43f6e1b1acc274759eedf651cdbf419864af8345
shiveshnavin <shiveshnavin@gmail.com>
2017-08-26 22:19:54 +0530

    added bottom bar

--- app/src/main/java/in/hoptec/exploman/PlaceDetails.java
+++ app/src/main/java/in/hoptec/exploman/PlaceDetails.java
@@ -1,13 +1,32 @@
 package in.hoptec.exploman;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.nineoldandroids.animation.Animator;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
 
 public class PlaceDetails extends AppCompatActivity {
+
+
+    @BindView(R.id.imageView)
+    ImageView flip;
+    public Context ctx;
+    public Activity act;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +36,90 @@ public class PlaceDetails extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
     }
+
+
+    Handler h;
+    Runnable r;
+    int dr=0;
+    ArrayList<String> strip;
+    public void flip()
+    {
+
+
+
+        h=new Handler();
+        r=new Runnable() {
+            @Override
+            public void run() {
+
+                if(dr==strip.size()-1)
+                {
+                    dr=0;
+                }
+
+                Picasso.with(ctx).load(strip.get(dr++)).into(flip);
+                // flip.setImageResource(d);
+                YoYo.with(Techniques.SlideInLeft).withListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animator) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animator) {
+
+
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                YoYo.with(Techniques.SlideOutRight).withListener(new Animator.AnimatorListener() {
+                                    @Override
+                                    public void onAnimationStart(Animator animator) {
+
+                                    }
+
+                                    @Override
+                                    public void onAnimationEnd(Animator animator) {
+                                        h.postDelayed(r, 10);
+                                    }
+
+                                    @Override
+                                    public void onAnimationCancel(Animator animator) {
+
+                                    }
+
+                                    @Override
+                                    public void onAnimationRepeat(Animator animator) {
+
+                                    }
+                                }).duration(200).playOn(flip);
+
+                            }
+                        },5000);
+
+                        //   h.postDelayed(r,5000);
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animator) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animator) {
+
+                    }
+                }).duration(200).playOn(flip);
+            }
+        };
+
+
+        h.postDelayed(r, 100);
+
+
+    }
+
+
+
 
 }
