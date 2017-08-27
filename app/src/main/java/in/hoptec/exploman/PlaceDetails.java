85113b9d98b06f19e82e73b4e928360165d54aa5
shiveshnavin <shiveshnavin@gmail.com>
2017-08-27 22:06:51 +0530

    added review

--- app/src/main/java/in/hoptec/exploman/PlaceDetails.java
+++ app/src/main/java/in/hoptec/exploman/PlaceDetails.java
@@ -7,21 +7,34 @@ import android.os.Handler;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatRatingBar;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.androidnetworking.interfaces.StringRequestListener;
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
 import com.nineoldandroids.animation.Animator;
 import com.squareup.picasso.Picasso;
+import com.wang.avi.AVLoadingIndicatorView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import in.hoptec.exploman.adapters.ReviewAdapter;
 import in.hoptec.exploman.database.Place;
+import in.hoptec.exploman.database.Review;
 import in.hoptec.exploman.views.GoalProgressBar;
 
 public class PlaceDetails extends AppCompatActivity {
@@ -30,6 +43,14 @@ public class PlaceDetails extends AppCompatActivity {
     ImageView flip;
 
 
+    @BindView(R.id.write_rev)
+    ImageView write_rev;
+
+    @BindView(R.id.review_plc)
+    LinearLayout review_plc;
+
+    @BindView(R.id.splash_view2)
+    AVLoadingIndicatorView loading;
 
     @BindView(R.id.prog)
     GoalProgressBar prog;
@@ -70,6 +91,7 @@ public class PlaceDetails extends AppCompatActivity {
     RecyclerView rec;
 
 
+    ArrayList<Review> reviews;
     public Context ctx;
     public Activity act;
 
@@ -93,10 +115,122 @@ public class PlaceDetails extends AppCompatActivity {
             fill(place);
         }
 
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+    public void write()
+    {}
+   final int LOADING=12,EMPTY=13,LOADED=14;
+
+
+     public void load(int state)
+    {
+
+        if(state==LOADING)
+        {
+            loading.setVisibility(View.VISIBLE);
+            rec.setVisibility(View.GONE);
+            review_plc.setVisibility(View.GONE);
+        }
+
+
+        if(state==EMPTY)
+        {
+            loading.setVisibility(View.GONE);
+            rec.setVisibility(View.GONE);
+            review_plc.setVisibility(View.VISIBLE);
+            review_plc.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    write();
+                }
+            });
+            utl.changeColorDrawable(write_rev,R.color.grey_500);
+
+
+        }
+
+
+        if(state==LOADED)
+        {
+            loading.setVisibility(View.GONE);
+            rec.setVisibility(View.VISIBLE);
+            review_plc.setVisibility(View.GONE);
+        }
 
 
     }
 
+
+    public void getReviews(String place_id)
+    {
+
+        load(LOADING);
+        String url=Constants.HOST+Constants.API_GET_PREVIEWS+"?place_id="+place_id;
+        AndroidNetworking.get(url).build().getAsJSONArray(new JSONArrayRequestListener() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                utl.l(response);
+
+                reviews=new ArrayList<Review>();
+                for(int i=0;i<response.length();i++)
+                {
+                    try {
+                        reviews.add(utl.js.fromJson(response.get(i).toString(),Review.class));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                setUpReviews(reviews);
+
+
+            }
+
+            @Override
+            public void onError(ANError ANError) {
+
+                utl.l(ANError.getErrorDetail());
+                load(EMPTY);
+            }
+        });
+
+
+
+    }
+    public void setUpReviews(ArrayList<Review> reviews)
+    {
+        if(reviews.size()==0)
+        {
+            load(EMPTY);
+            return;
+        }
+        load(LOADED);
+
+        ReviewAdapter adapter=new ReviewAdapter(ctx, reviews, new ReviewAdapter.CallBacks() {
+            @Override
+            public void share(Review cat, int id) {
+
+            }
+
+            @Override
+            public void like(Review cat, boolean like) {
+
+            }
+
+            @Override
+            public void click(Review cat, int id, View v) {
+
+            }
+        });
+        rec.setLayoutManager(new LinearLayoutManager(ctx));
+        rec.setAdapter(adapter);
+
+
+
+    }
     public void fill(Place pla)
     {
 
@@ -108,6 +242,7 @@ public class PlaceDetails extends AppCompatActivity {
         strip=pla.getImages();
         flip();
 
+        getReviews(pla.id);
 
 
 
@@ -122,6 +257,12 @@ public class PlaceDetails extends AppCompatActivity {
     {
 
 
+        if(strip.size()<=1)
+        {
+
+            Picasso.with(ctx).load(strip.get(dr)).placeholder(R.drawable.placeholder).into(flip);
+            return;
+        }
 
         h=new Handler();
         r=new Runnable() {
